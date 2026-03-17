ec5d939cfb57e0e824e953c2aa53de3cb1cac1d5
Tanushree Pal <tanushreepal2005@gmail.com>
2026-03-17 07:49:22 +0530

    Solved Left Rotate Array by K Places

---
+++ Left Rotate Array by K Places.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        if (n<k) k=k%n;
+        reverse(nums.begin(),nums.begin()+n);
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.begin()+n);
+    }
+};
