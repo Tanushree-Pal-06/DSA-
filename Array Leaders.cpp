344cc22e1a68dd76426819f27a8d5fdd05b1be0d
Tanushree Pal <tanushreepal2005@gmail.com>
2026-03-31 12:35:12 +0530

    Solved Array Leaders

---
+++ Array Leaders.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // Code here
+        vector <int> ans;
+        int n=arr.size();
+        int maxi=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if (arr[i]>=maxi){
+                ans.push_back(arr[i]);
+            }
+            maxi=max(maxi,arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
