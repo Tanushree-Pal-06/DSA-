3e6c9fef27ba22d939ebdcbf0c52eb855e4b0f64
Tanushree Pal <tanushreepal2005@gmail.com>
2026-06-11 08:17:19 +0530

    Solved Implement stack using array.

---
+++ Stack and Queues/Implement stack using array.cpp
@@ -0,0 +1,42 @@
+class myStack {
+  public:
+    vector<int> arr;
+    int top=-1;
+    int size;
+    myStack(int n) {
+        // Define Data Structures
+        arr.resize(n);
+        size=n;
+    }
+
+    bool isEmpty() {
+        // check if the stack is empty
+        return top==-1;
+    }
+
+    bool isFull() {
+        // check if the stack is full
+        return top==size-1;
+    }
+
+    void push(int x) {
+        // inserts x at the top of the stack
+       if (isFull()) return;
+
+        arr[++top] = x;
+    }
+
+    void pop() {
+        // removes an element from the top of the stack
+         if (isEmpty()) return;
+
+        top--;
+    }
+
+    int peek() {
+        // Returns the top element of the stack
+        if (isEmpty()) return -1;
+
+        return arr[top];
+    }
+};
