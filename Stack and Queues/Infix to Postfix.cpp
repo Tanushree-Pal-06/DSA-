94cb655089cf3e61cae2275f522d71d158a81ffe
Tanushree Pal <tanushreepal2005@gmail.com>
2026-06-14 17:39:27 +0530

    Solved Infix to Postfix.

---
+++ Stack and Queues/Infix to Postfix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    int priority(char op){
+        if(op=='^')return 3;
+        else if(op=='*' || op=='/')return 2;
+        else if(op=='+' || op=='-')return 1;
+        return -1;
+    }
+    string infixToPostfix(string& s) {
+        // code here
+        string ans="";
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if((s[i]>='A'&& s[i]<='Z') ||
+            (s[i]>='a'&& s[i]<='z') ||
+            (s[i]>='0'&& s[i]<='9') 
+            ) {ans+=s[i];}
+            
+            else if(s[i]=='(')st.push(s[i]);
+            
+            else if(s[i]==')'){
+                while(!st.empty() && st.top()!='('){
+                 
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.pop();
+            }
+            else{
+                while(!st.empty() &&
+      (priority(s[i]) < priority(st.top()) ||
+      (priority(s[i]) == priority(st.top()) && s[i] != '^'))){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+        }
+        
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
