6f69595b589152673e2a64e9f1a1150884d4add2
Tanushree Pal <tanushreepal2005@gmail.com>
2026-06-15 07:00:28 +0530

    Solved Postfix to Prefix.

---
+++ Stack and Queues/Postfix to Prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    string postToPre(string s) {
+        // code here
+        stack<string> st;
+        for(int i=0;i<s.size();i++){
+            if(((s[i]>='A'&& s[i]<='Z')||
+            (s[i]>='a' && s[i]<='z') ||
+            (s[i]>='0' && s[i]<='9'))){
+                st.push(string(1,s[i]));
+            }
+            else{
+                char op=s[i];
+                string s1=st.top();
+                st.pop();
+                string s2=st.top();
+                st.pop();
+                st.push(op+s2+s1);
+            }
+        }
+        return st.top();
+    }
+};
