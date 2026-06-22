1cf9bcefb68246fb4c8be41c897d274b3472c99f
Tanushree Pal <tanushreepal2005@gmail.com>
2026-06-22 07:49:16 +0530

    Solved Binary Tree Maximum Path Sum.

---
+++ Binary Trees/Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findmaxi(TreeNode* root, int &maxi){
+        if (root==NULL) return 0;
+        int ls=max(0,findmaxi(root->left,maxi));
+        int rs=max(0,findmaxi(root->right,maxi));
+
+        maxi=max(maxi,ls+rs+root->val);
+
+        return root->val+max(ls,rs);
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int maxi=INT_MIN;
+        findmaxi(root,maxi);
+        return maxi;
+    }
+    
+};
