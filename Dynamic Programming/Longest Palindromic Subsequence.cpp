258e074f70ab9f26ab8bc09cb9dde87ae5323946
Tanushree Pal <tanushreepal2005@gmail.com>
2026-07-15 08:14:31 +0530

    Solved Longest Palindromic Subsequence.

---
+++ Dynamic Programming/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+     int lcs(string &s1, string &s2) {
+        // code here
+        
+        int n=s1.size();
+        int m=s2.size();
+        
+        vector<int> prev(m+1,0);
+        vector<int> cur(m+1,0);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) cur[j]=1+prev[j-1];
+                else cur[j]=max(prev[j],cur[j-1]);
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return lcs(s,t);
+    }
+};
