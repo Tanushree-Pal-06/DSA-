ad31d0eb0f0763cc7191f3c96ce136ff417eb732
Tanushree Pal <tanushreepal2005@gmail.com>
2026-07-17 08:34:27 +0530

    Solved  Minimum in BST

---
+++ Binary Search Trees/Minimum in BST.cpp
@@ -0,0 +1,27 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // code here
+        if(root==NULL)return -1;
+        while(root->left){
+         root=root->left;
+        }
+         return root->data;
+    }
+    
+};
