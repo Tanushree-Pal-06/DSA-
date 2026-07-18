383dc57e264a2db9b406e7ef38e47261f0aa5ff1
Tanushree Pal <tanushreepal2005@gmail.com>
2026-07-18 08:20:38 +0530

    Solved Ceil in BST.

---
+++ Binary Search Trees/Ceil in BST.cpp
@@ -0,0 +1,35 @@
+/*
+Definition for Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int findCeil(Node* root, int x) {
+        // code here
+        int ans=-1;
+        if(root==NULL)return ans;
+        
+        while(root!=NULL){
+            if(root->data==x)return root->data;
+            else if(root->data>x){
+                ans=root->data;
+                root=root->left;
+            }
+            else{
+                root=root->right;
+            }
+        }
+        return ans;
+    }
+};
