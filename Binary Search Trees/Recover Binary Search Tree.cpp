3449a62cd98130140309916a8b257a42ac1ad3cb
Tanushree Pal <tanushreepal2005@gmail.com>
2026-07-22 07:49:57 +0530

    Solved Recover Binary Search Tree.

---
+++ Binary Search Trees/Recover Binary Search Tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* first;
+    TreeNode* last;
+    TreeNode* prev;
+    TreeNode* middle;
+
+    void inorder(TreeNode* root){
+        if(root==NULL)return;
+        inorder(root->left);
+        if(prev!=NULL && root->val<prev->val){
+            if(first==NULL){// first violation
+                first=prev;
+                middle=root;
+            }
+            else{ //second violation
+                last=root;
+            }
+
+        }
+        prev=root;
+        inorder(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        first=last=middle=NULL;
+        inorder(root);
+        if(first && last) swap(first->val,last->val);
+        else if(first&& middle)swap(first->val,middle->val);
+    }
+};
