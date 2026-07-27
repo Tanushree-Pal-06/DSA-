55dfcf7eb79fea4a34537d7c7c20b2084d7171b8
Tanushree Pal <tanushreepal2005@gmail.com>
2026-07-27 08:40:54 +0530

    Solved Best Time to Buy and Sell Stock III.

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>after (2,vector<int> (3,0));
+        vector<vector<int>>cur (2,vector<int> (3,0));
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy==1){
+                        cur[buy][cap]=max(-prices[i]+after[0][cap],0+after[1][cap]);
+                    }
+                    else{
+                        cur[buy][cap]=max(prices[i]+after[1][cap-1],0+after[0][cap]);
+                    }
+                }
+            }
+            after=cur;
+
+        }
+        return after[1][2];
+    }
+};
